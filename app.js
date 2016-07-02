019f6d2042954a30159d291a78b02ea17259d962
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-02 19:33:09 +0900

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/',function (req,res) {
+  res.writeHead({'Content-Type':'text/html'});
+  res.write('<font size="200" color="green">my node.js server</font>');
+  res.end();
   res.send('Welcome to Hello World!');
 });
 
