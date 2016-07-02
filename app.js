5353cc60d0d677d006ebaa087373e3a4d9d1bc74
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-02 18:05:09 +0900

    first commit

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ var express = require('express');
 
 var app = express();
 
-var ward="Hello World!";
-
 app.get('/',function (req,res) {
-  res.send(ward());
+  res.send('Hello World!');
 });
 
 app.listen(3000, function(){
-  console.log('Server On!\n');
-  console.log(ward);
+  console.log('Server On!');
 });
