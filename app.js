c9ba387eb9264e0e6b04e344e2f74db0631398ce
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-03 16:24:02 +0900

    first commit

--- app.js
+++ app.js
@@ -2,17 +2,6 @@ var express = require('express');
 
 var app = express();
 
-function draw(){
-  ctx.save();
-  ctx.font = "20px Arial";
-  ctx.fillStyle = "black";
-  ctx.fillText("Welcome to Hello World!");
-  ctx.restore();
-}
-app.get('/',function (req,res) {
-  res.font = "200px Arial";
-  res.fillStyle = "red";
-  mystyle.fontSize=red;
   res.send('Welcome to Hello World!');
 });
 
