bc3dc7ad5736b3a66acde5e3eff783f94508d910
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-03 18:06:23 +0900

    first commit

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express();
 
+app.get('/',function (req.res) {
   res.send('Welcome to Hello World!');
-});
+}
 
 app.listen(3000, function(){
   console.log('Server On!');
