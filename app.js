6da8aec8d19ec3dee8a6d26fda8853e05477534d
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-06 18:47:08 +0900

    first commmit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb://careb:car.eb@ds013004.mlab.com:13004/mjhmjh1104");
+mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open",function () {
   console.log("DB connected!");
