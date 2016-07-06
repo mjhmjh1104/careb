6af897bb23d055264845f7e820f0f49cd424062e
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-06 18:39:56 +0900

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express');
-
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://careb:car.eb@ds013004.mlab.com:13004/mjhmjh1104");
+var db = mongoose.connection;
+db.once("open",function () {
+  console.log("DB connected!");
+});
+db.on("error",function (err) {
+  console.log("DB ERROR :", err);
+});
 
 app.get('/',function (req,res) {
   res.send('Welcome to Hello World!');
