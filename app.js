7bc5b91a97beeff03982b65f987f5d97fc314e8e
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-16 16:08:03 +0900

    first commit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.get('/',function (req,res) {
 );
 
 app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
