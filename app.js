acbd3cb4ec993a75e32b5f372cec70d62f015191
mjhmjh1104 <mjhmjh1104@naver.com>
2016-07-18 18:49:04 +0900

    first commit

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ db.on("error",function (err) {
 
 app.get('/',function (req,res) {
   res.send('여기는 car.eb 서버입니다. 개발자 Ender_Dragon1104, Bluechicken0520님이 열심히 만들고 있으니 완성되면 한 번 놀러 와보세요!!!');
+  res.send('+ 7/18 6:48:42 : 에러가 발견되었네요. 빠른 시일 내에 고치도록 노력하겠습니다.')
 }
 );
 
